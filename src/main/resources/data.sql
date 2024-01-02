b2cf814c2cbbc6512f122c8d86b4b689e448ee19
AtsushiNi <atsushi9731@gmail.com>
2024-01-02 21:25:30 +0900

    commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into issues (summary, description) values ('バグA', 'バグがあります');
 insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
 insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
+
+insert into users (username, password, authority) values ('tom', '7105cb7d0da69b0a0eeb5b067cba409c2ab2b255e026075fe6aff3f3b3249c33c282192e8d87904e', 'ADMIN');
+insert into users (username, password, authority) values ('bob', '7105cb7d0da69b0a0eeb5b067cba409c2ab2b255e026075fe6aff3f3b3249c33c282192e8d87904e', 'USER');
